27e8f73e6fdce5eb05ef94397ebc3cf08d8ba73b
Sebastian Richter <sebastian.richter@paessler.com>
2019-05-19 14:23:24 +0200

    -given dt in on_update is now a floating number. -resolved all warnings regarding unused Result. -on_update now returns a result so the main loop can handle errors. -extended the hello world example.

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,33 @@ mod data;
 mod pix;
 
 use crate::pix::{run, screen, PixLifecycle, PixSettings, PixWindow};
-use sdl2::event::Event;
 use sdl2::pixels::Color;
-use std::time::Duration;
+use std::f32::consts::PI;
 
-struct Game;
+struct Game {
+  t: f32
+}
 
 impl PixLifecycle for Game {
   fn on_init(&self) -> PixSettings {
     screen(256, 240, "PiX - Example")
   }
-  fn on_update(&mut self, window: &mut PixWindow, dt: u32) {
+  fn on_update(&mut self, window: &mut PixWindow, dt: f32) -> Result<(), String> {
+    let (w, h) = window.dimensions();
+    let w = w as f32;
+    let h = h as f32;
+    self.t += dt * 1.25;
+    while self.t >= PI * 2.0 {
+      self.t -= PI * 2.0;
+    }
+    let x = w / 2.0 - (11.0 * 8.0 / 2.0);
+    let y = h / 2.0 + f32::sin(self.t) * h / 4.0 - 4.0;
     window.clear(Color::RGB(0, 0, 0));
-    window.print(Color::RGB(255, 255, 255), 10, 10, "Hello World");
-    window.draw_line(0, 0, 10, 20, Color::RGB(255, 255, 255));
+    window.print(Color::RGB(255, 255, 255), x as i32, y as i32, "Hello World")?;
+    Ok(())
   }
 }
 
 fn main() -> Result<(), String> {
-  run(Game)
+  run(Game { t: 0.0 })
 }
--- src/pix.rs
+++ src/pix.rs
@@ -1,8 +1,7 @@
 use crate::data::FONT8X8;
 use sdl2::event::Event;
-use sdl2::hint::set;
 use sdl2::pixels::Color;
-use sdl2::rect::{Point, Rect};
+use sdl2::rect::Point;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 use sdl2::Sdl;
@@ -21,18 +20,18 @@ pub struct PixWindow {
 pub trait PixLifecycle: 'static {
   fn on_init(&self) -> PixSettings {
     PixSettings {
-      width: 800,
-      height: 600,
+      width: 256,
+      height: 240,
       title: "Default",
     }
   }
-  fn on_update(&mut self, window: &mut PixWindow, dt: u32) {}
+  fn on_update(&mut self, window: &mut PixWindow, dt: f32) -> Result<(), String>;
 }
 
 impl PixWindow {
-  pub fn new(sdl_ctx: &Sdl, width: u32, height: u32, title: &str) -> Self {
-    let video_ctx = sdl_ctx.video().unwrap();
-    let display_mode = video_ctx.desktop_display_mode(0).unwrap();
+  pub fn new(sdl_ctx: &Sdl, width: u32, height: u32, title: &str) -> Result<Self, String> {
+    let video_ctx = sdl_ctx.video()?;
+    let display_mode = video_ctx.desktop_display_mode(0)?;
     let factor = std::cmp::min(
       display_mode.w / width as i32,
       display_mode.h / height as i32,
@@ -40,13 +39,16 @@ impl PixWindow {
     let new_width = width * factor as u32;
     let new_height = height * factor as u32;
     let window = video_ctx
-      .window(title, new_width, new_height)
-      .position_centered()
-      .build()
-      .unwrap();
-    let mut canvas = window.into_canvas().build().unwrap();
-    canvas.set_logical_size(width, height);
-    PixWindow { canvas }
+        .window(title, new_width, new_height)
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())?;
+    canvas.set_logical_size(width, height).map_err(|e| e.to_string())?;
+    Ok(PixWindow { canvas })
   }
 
   pub fn clear(&mut self, color: Color) {
@@ -54,12 +56,13 @@ impl PixWindow {
     self.canvas.clear();
   }
 
-  pub fn draw_pixel(&mut self, x: i32, y: i32, color: Color) {
+  pub fn draw_pixel(&mut self, x: i32, y: i32, color: Color) -> Result<(), String> {
     self.canvas.set_draw_color(color);
-    self.canvas.draw_point(Point::new(x, y));
+    self.canvas.draw_point(Point::new(x, y))?;
+    Ok(())
   }
 
-  pub fn draw_line(&mut self, x0: i32, y0: i32, x1: i32, y1: i32, color: Color) {
+  pub fn draw_line(&mut self, x0: i32, y0: i32, x1: i32, y1: i32, color: Color) -> Result<(), String> {
     let mut x0 = x0;
     let mut y0 = y0;
     let dx = i32::abs(x1 - x0);
@@ -67,22 +70,21 @@ impl PixWindow {
     let sx = if x0 < x1 { 1 } else { -1 };
     let sy = if y0 < y1 { 1 } else { -1 };
     let mut err = if dx > dy { dx / 2 } else { -dy / 2 };
-    let mut e2 = 0;
     'running: loop {
-      self.draw_pixel(x0, y0, color);
+      self.draw_pixel(x0, y0, color)?;
       if x0 == x1 && y0 == y1 {
         break 'running;
       }
-      e2 = err;
-      if e2 > -dx {
+      if err > -dx {
         err -= dy;
         x0 += sx;
       }
-      if e2 < dy {
+      if err < dy {
         err += dx;
         y0 += sy;
       }
     }
+    Ok(())
   }
 
   pub fn draw_rect(
@@ -93,7 +95,7 @@ impl PixWindow {
     mut y1: i32,
     color: Color,
     fill: bool,
-  ) {
+  ) -> Result<(), String> {
     if x0 > x1 {
       std::mem::swap(&mut x0, &mut x1);
     }
@@ -103,22 +105,30 @@ impl PixWindow {
     if fill {
       for y in y0..y1 + 1 {
         for x in x0..x1 + 1 {
-          self.draw_pixel(x, y, color)
+          self.draw_pixel(x, y, color)?;
         }
       }
     } else {
       for y in y0..y1 + 1 {
-        self.draw_pixel(x0, y, color);
-        self.draw_pixel(x1, y, color);
+        self.draw_pixel(x0, y, color)?;
+        self.draw_pixel(x1, y, color)?;
       }
       for x in x0..x1 + 1 {
-        self.draw_pixel(x, y0, color);
-        self.draw_pixel(x, y1, color);
+        self.draw_pixel(x, y0, color)?;
+        self.draw_pixel(x, y1, color)?;
       }
     }
+    Ok(())
   }
 
-  pub fn draw_circle(&mut self, x: i32, y: i32, radius: i32, color: Color, fill: bool) {
+  pub fn draw_circle(
+    &mut self,
+    x: i32,
+    y: i32,
+    radius: i32,
+    color: Color,
+    fill: bool,
+  ) -> Result<(), String> {
     let r0sq = if fill { 0 } else { (radius - 1) * (radius - 1) };
     let r1sq = radius * radius;
     let x0 = x;
@@ -131,13 +141,14 @@ impl PixWindow {
         let dx = x * x;
         let distance = dx + dy;
         if distance >= r0sq && distance <= r1sq {
-          self.draw_pixel(x0 + x, y0 + y, color);
+          self.draw_pixel(x0 + x, y0 + y, color)?;
         }
       }
     }
+    Ok(())
   }
 
-  pub fn print(&mut self, color: Color, x: i32, y: i32, text: &str) {
+  pub fn print(&mut self, color: Color, x: i32, y: i32, text: &str) -> Result<(), String> {
     let mut x0 = x;
     let y0 = y;
     for char in text.chars() {
@@ -145,12 +156,13 @@ impl PixWindow {
         let mask = FONT8X8[char as usize][y as usize];
         for x in 0..8 {
           if (mask & (1 << x)) != 0 {
-            self.draw_pixel(x0 + x, y0 + y, color);
+            self.draw_pixel(x0 + x, y0 + y, color)?;
           }
         }
       }
       x0 += 8;
     }
+    Ok(())
   }
 
   pub fn dimensions(&self) -> (u32, u32) {
@@ -162,7 +174,7 @@ pub fn run<E: PixLifecycle>(mut lifecycle: E) -> Result<(), String> {
   let settings = lifecycle.on_init();
   let sdl_ctx = sdl2::init().unwrap();
   let mut sdl_timer = sdl_ctx.timer().unwrap();
-  let mut window = PixWindow::new(&sdl_ctx, settings.width, settings.height, settings.title);
+  let mut window = PixWindow::new(&sdl_ctx, settings.width, settings.height, settings.title)?;
   let mut event_pump = sdl_ctx.event_pump()?;
   let mut last_tick = 0;
   'running: loop {
@@ -174,13 +186,13 @@ pub fn run<E: PixLifecycle>(mut lifecycle: E) -> Result<(), String> {
         _ => {}
       }
     }
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    // The rest of the game loop goes here...
     let current_tick = sdl_timer.ticks();
     let delta_tick = current_tick - last_tick;
     last_tick = current_tick;
-    lifecycle.on_update(&mut window, delta_tick);
+    lifecycle.on_update(&mut window, delta_tick as f32 / 1000.0)?;
     window.canvas.present();
-    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    // The rest of the game loop goes here...
   }
   Ok(())
 }
